dac017270cae1515b20d00ad83f8bcfc901a3fef
courcoa <nono.cx@outlook.com>
2025-05-13 20:18:12 +0200

    Update script_formatage_fiche_client.user.js

--- script_formatage_fiche_client.user.js
+++ script_formatage_fiche_client.user.js
@@ -4,7 +4,6 @@
 // @version      2.91
 // @description  Corrige nom/prénom, coche mail & sms, affiche un bandeau, compatible souris + tactile
 // @match        https://salon2.hairnet.fr/management/fiche-client.php*
-// @updateURL    https://courcoa.github.io/tampermonkey_salons/script_formatage_fiche_client.user.js
 // @downloadURL  https://courcoa.github.io/tampermonkey_salons/script_formatage_fiche_client.user.js
 // @grant        none
 // ==/UserScript==
